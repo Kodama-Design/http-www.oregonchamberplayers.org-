cc9ee2b7a29bc64e759b829c12bc7d6c39bc9d51
Robert Parker <rparker@kodama.design>
2018-02-03 00:52:09 -0800

    disable jquery replacement and add more styles

--- wp-content/themes/ocp/functions.php
+++ wp-content/themes/ocp/functions.php
@@ -3,7 +3,7 @@ require __DIR__.'/vendor/autoload.php';
 
 $container = (new \OCP\Bootstrap)->getContainer();
 (new \OCP\Yoast())->low();
-(new \OCP\Jquery())->replace("3.2.1");
+//(new \OCP\Jquery())->replace("3.2.1");
 (new \OCP\AdminBar())->removeMargin();
 (new \OCP\Login())->addInlineStyles("body {color: #fff;}");
 (new \OCP\Translate())->loadTextDomain($container->getParameter('translation.path'));
